c0a2f196b3a1d47f5eb74b76b603638c4cf46b37
jose casas <casasnaranjo@hotmail.com>
2016-12-31 00:06:46 -0500

    Configuracion de Multipart/form en cliente web Api para subir imagenes para ser reconocidas.

--- ClienteConsola/Program.cs
+++ ClienteConsola/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,19 +25,25 @@ namespace ClienteConsola
             {
                 try
                 {
-                    var httpclient = new HttpClient();
-                    string URI = "http://localhost:38107/api/EmoUploaderAPI";
-                    var filestream = File.OpenRead("c:/3.jpg");
-                    StreamContent Contenido = new StreamContent(filestream);
 
-                    //ntenido.Headers.ContentDisposition.FileName = filestream.Name;
-                    var Respuesta = await httpclient.PostAsync(URI, Contenido);
+                    string URI = "http://localhost:38107/api/EmoUploaderAPI";
+                  
+
+                    var httpClient = new HttpClient();
+                    httpClient.DefaultRequestHeaders.TransferEncodingChunked = true;
+
+                    var content = new MultipartFormDataContent();
+                    var imageContent = new StreamContent(new FileStream("c:/3.jpg", FileMode.Open, FileAccess.Read, FileShare.Read));
+                    imageContent.Headers.ContentType = MediaTypeHeaderValue.Parse("image/jpeg");
+                    content.Add(imageContent, "3", "c:/3.jpg");
+
+                    var Respuesta=await httpClient.PostAsync(URI, content);
                     // var Respuesta = await httpclient.GetAsync(URI);
                     Respuesta.EnsureSuccessStatusCode();
                     var contenido = await Respuesta.Content.ReadAsStringAsync();
                     Console.WriteLine("respuesta get a" + URI + "" + Environment.NewLine + (contenido.Replace("{", Environment.NewLine + "\t{")).Replace("[", "[" + Environment.NewLine + "\t\t "));
                     Console.WriteLine("Programa para obtener Post API");
-                    filestream.Close();
+
                     Console.ReadLine();
 
                 }
